7ef4357037fe4903878b83dc3c57f33daab2c6b0
AhsanJafri <44376846+AhsanJafri@users.noreply.github.com>
2019-07-13 16:14:26 +0500

    Create IndexPage.php

---
+++ IndexPage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Page</title>
+</head>
+<body>
+	<form action="mcqs.php" method="POST">
+		1= Math <br>
+		2= Physic<br>
+		3= Chem<br>
+		4= Urdu<br><br><br>
+		<input type="text" name="Sub">&nbspSubject</input><br><br>
+		<select name="opt">
+  		<option >2019</option>
+  		<option >2018</option>
+  		<option >2017</option>
+  		<option >2016</option>
+		</select>&nbsp Year<br><br><br>
+		<input type="Submit" name="submit">
+	</form>
+</body>
+</html>
