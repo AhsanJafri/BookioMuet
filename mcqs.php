69e367db3bc17b72f269e9439833e92c402d5602
AhsanJafri <44376846+AhsanJafri@users.noreply.github.com>
2019-07-16 15:34:07 +0500

    Update mcqs.php

--- mcqs.php
+++ mcqs.php
@@ -1,41 +1,58 @@
 <?php
 	
-  include('Databaseinclude.php');
-
+ 	$conn=mysqli_connect('localhost','root','','bookio');
+	
+	$z=1;
+	$a=1;
+	$i=array();												#Intialize an array
+	$t=array();
 	#Taking the input from the firstpage.php
 	if(isset($_POST['submit'])){
 		$year=$_POST['opt'];
 		$sub=$_POST['Sub'];
-	}
 		#$rowcount=mysqli_num_rows($row);
 		#$ran=rand(1,$rowcount);
-		$i=array();												#Intialize an array
-		$t=array();
+		
 		$query="Select * from mcqs where Year='$year'";			#Query which we wanted To execute
 		if($result=mysqli_query($conn,$query))					#Fetching out the result
 		{
 			echo "<form method='POST'>";
-		
+			
 			while ($ques=mysqli_fetch_assoc($result))      
 			{
+				
+				echo $z;
 				#echo $ran."<br>";			 
 				echo "<h1>".$ques['M_ques']."</h1><br>";
-				echo "<input type='radio' name='Ans'>".$ques['optionone']."<br>";
-				echo "<input type='radio' name='Ans'>".$ques['optiontwo']."<br>";
-				echo "<input type='radio' name='Ans'>".$ques['optionthree']."<br>";
+				echo "<input type='radio' name=".$z.">".$ques['optionone']."</input><br>";
+				echo "<input type='radio' name=".$z.">".$ques['optiontwo']."</input><br>";
+				echo "<input type='radio' name=".$z.">".$ques['optionthree']."</input><br>";
+				//echo "<input type='hidden' name=".$a." value=".$ques['answer']."><br>";
 				array_push($i,strval($ques['answer']));
-			}
 
+				$z++;
+				$a++;
+			}
 			#Now after fetching we wanted to verify our anwer
-			echo "<input type='submit' name='sub'></input></form>";
-					if(isset($_POST['sub'])){
-
-						$Ans=$_POST['Ans'];
-						echo $Ans;			 
-
+			echo "<br><input type='submit' name='sub' value='Submit' ></form>";
+					
+							
+		}
+	}
 			
-						}		
-			}
 	
 
+						if(isset($_POST['sub'])){
+							$ans1=$_POST['1'];
+							echo $ans1;
+			
+						}
 ?>
+<script type="text/javascript">
+		var input= <?php echo json_encode($i) ?>;
+		document.write(input);
+		var array1 =new Array();
+		for (var i=1,i<input,i++) {
+			var a=document.getElementById(i);
+			array1.append(a);}
+</script>
